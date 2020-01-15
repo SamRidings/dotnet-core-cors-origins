6267c0c9dbfbf483b9819e6e694b26be7ca89bbc
Samuel Ridings <samuel.ridings@gmail.com>
2020-01-15 19:15:43 +0000

    first commit

---
+++ Startup.cs
@@ -0,0 +1,61 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Cors_Origins_Example
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton(Configuration);
+            services.AddControllers();
+
+            var hosts = Configuration.GetSection("Cors:AllowedOrigins").Get<List<string>>();
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", p =>
+                {
+                    p.AllowAnyHeader()
+                    .AllowAnyMethod()
+                    .WithOrigins(hosts.ToArray())
+                    .AllowCredentials();
+                });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseCors("CorsPolicy");
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
